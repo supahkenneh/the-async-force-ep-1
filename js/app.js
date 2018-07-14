7463623538e6bf455d6222906ef1c40b81cc4bdc
Kenny Chung <kcmac@Kennys-MacBook-Pro.local>
2018-07-13 14:46:25 -1000

    vader and solo

---
+++ js/app.js
@@ -0,0 +1,65 @@
+const darthVader = new XMLHttpRequest();
+darthVader.open('GET', "https://swapi.co/api/people/4/");
+darthVader.send();
+
+
+darthVader.addEventListener('load', function () {
+  const parseDarthVader = JSON.parse(this.responseText);
+  document.getElementById('person4Name').innerHTML = parseDarthVader.name;
+
+});
+
+const darthVaderHome = new XMLHttpRequest();
+darthVaderHome.open('GET', "https://swapi.co/api/planets/1/");
+darthVaderHome.send();
+
+darthVaderHome.addEventListener('load', function () {
+  const parseDarthVaderHome = JSON.parse(this.responseText);
+  document.getElementById('person4HomeWorld').innerHTML = 'from ' + parseDarthVaderHome.name;
+})
+
+const hanSolo = new XMLHttpRequest();
+hanSolo.open('GET', "https://swapi.co/api/people/14");
+hanSolo.send();
+
+hanSolo.addEventListener('load', function () {
+  const parseHanSolo = JSON.parse(this.responseText);
+  document.getElementById('person14Name').innerHTML = parseHanSolo.name;
+})
+
+const hanSoloSpecies = new XMLHttpRequest();
+hanSoloSpecies.open('GET', "https://swapi.co/api/species/1");
+hanSoloSpecies.send();
+
+hanSoloSpecies.addEventListener('load', function () {
+  const parseHanSoloSpecies = JSON.parse(this.responseText);
+  document.getElementById('person14Species').innerHTML = 'is a ' + parseHanSoloSpecies.name;
+})
+
+const getFilmList = new XMLHttpRequest();
+getFilmList.open('GET', "https://swapi.co/api/films/");
+getFilmList.send();
+
+
+getFilmList.addEventListener('load', function () {
+  const parseFilmList = JSON.parse(this.responseText);
+  const filmList = document.getElementById('filmList');
+
+  for (let i = 0; i < parseFilmList.results.length; i++) {
+
+    const newFilm = document.createElement('li');
+    newFilm.className = 'film';
+    filmList.appendChild(newFilm);
+
+    const filmHeader = document.createElement('h2');
+    newFilm.appendChild(filmHeader);
+    filmHeader.innerHTML = parseFilmList.results[i].title;
+  }
+})
+
+const getPlanetList = new XMLHttpRequest();
+getPlanetList.open('GET', );
+getPlanetList.send();
+
+
+
